42416a2b12096e501f4f49129bc1e9f28a3f8d90
R A <R A>
2017-04-29 23:17:15 -0600

    add myWeb folder

---
+++ web/myWeb/home.php
@@ -0,0 +1,66 @@
+<?php 
+echo "<!DOCTYPE html>\n";
+echo "<html>\n";
+echo "<head>\n\n";
+
+include 'header.php';
+
+echo "</head>\n";
+echo "<body>\n";
+echo "<header>\n\n";
+
+include 'menu.php';
+
+echo "</header>\n";
+echo "	<main>\n";
+echo "		<div>\n";
+echo "			<p class=\"courier bold bglightblue\">Our Mission:</p>\n"; 
+echo "			<hr/>\n\n";
+
+echo "			<img id=\"sad_pic\" src = \"sad.jpg\" alt = \"sad\"/>\n\n";  
+
+echo "			<p class=\"fontVerdana\">\n";
+echo "			Landon Jamieson is a poor CS major without a laptop.\n";
+echo "			<br/>\n";
+echo "			<br/>\n";
+echo "			Our <span class=\"bold\"> mission</span>  is to\n";
+echo "			get him a laptop.\n";
+echo "			<br/>\n";
+echo "			<br/>\n";          
+echo "			He just can't make it \n";
+echo "			without <span class=\"bold inline-color-text\">your\n";
+echo "  			help.</span>\n";
+echo "			</p>\n";
+
+echo "			<br/>\n";
+echo "			<br/>\n";
+echo "			<br/>\n";
+echo "		</div>\n";
+
+echo "		<div>\n";
+echo "			<h1>Our Goal:</h1>\n";
+echo "		</div>\n";
+
+echo "		<hr/>\n";
+
+echo "		<div>\n";
+echo "   		<a href = \"http://bit.ly/1dcJlUd\">\n";
+echo "      	<img id=\"hp-pic\" src=\"hp.jpg\" alt=\"Laptop to buy\">\n";
+echo "   		</a>\n";
+
+echo "   	<div id=\"laptop-info\">\n";
+echo "      	<p>\n";
+echo "        	Our goal is to purchase Landon a <span class=\"bold\">HP Flyer\n";
+echo "         	 Red 15.6\" Laptop PC</span>. For more \n";
+echo "        	<br/>\n";
+echo "        	<br/>\n";
+echo "        	information about this computer click on the picture.\n";
+echo "      	</p>\n";
+echo "   	</div>\n";
+
+echo "		</div>\n";
+		
+echo "	</main>\n";
+echo "</body>\n";
+echo "</html>\n";
+?>
